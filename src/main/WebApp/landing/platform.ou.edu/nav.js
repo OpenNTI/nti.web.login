9c327378aff1270b3dd605548785d4135c56c5e7
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-10-20 20:04:17 +0000

    add animation transitions (per aaron)
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtLoginApp/trunk@25896 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/WebApp/landing/platform.ou.edu/nav.js
+++ src/main/WebApp/landing/platform.ou.edu/nav.js
@@ -14,20 +14,43 @@ $(function(){
 
 	}
 	
+	function delayedRemove(jQ,name,hide){
+		setTimeout(function(){
+			jQ.removeClass(name);
+			if(hide){
+				jQ.hide();
+			}
+		},1200);
+	}
+	
 	function showPage(){
 		$('body').scrollTop(0)
 		var view = location.hash.substr(1),
-			base = $('#wrapper-all,#footer');
+			base = $('#wrapper-all,#footer'),
+			direction, current, next;
 
 		$('.subpage:visible iframe').each(stopVideosHack);
-		$('.subpage').hide().removeClass('hidden');
-		
+		current = $('.subpage:visible').index();
+
 		if(view){
+			next = $('#'+view).index();
+			direction = (next - current) > 0;
+
+			inDir = direction ? 'Right':'Left';
+			outDir = !direction ? 'Right':'Left';
+
+			
+			
+			delayedRemove( $('.subpage:visible').addClass('slideOut'+outDir), 'shown slideOut'+outDir, true);
+
 			base.find(':not(.subpage) * iframe').each(stopVideosHack);
-			base.hide();
-			$('#'+view).show();
+
+			delayedRemove( base, '', true );			
+			delayedRemove( $('#'+view).addClass('shown slideIn'+inDir).show(),'slideIn'+inDir);
 			return;
 		}
+		
+		delayedRemove( $('.subpage:visible').addClass('slideOutDown'), 'slideOutDown shown', true);
 		base.show();
 	}
 	
