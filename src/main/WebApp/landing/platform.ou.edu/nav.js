4e3c50bbcbea9b08b4c70bdce22dfc8e6d7d9e1d
Jonathan Grimes <jonathan.grimes@nextthought.com>
2013-10-20 05:11:51 +0000

    stop videos
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtLoginApp/trunk@25886 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/WebApp/landing/platform.ou.edu/nav.js
+++ src/main/WebApp/landing/platform.ou.edu/nav.js
@@ -1,12 +1,29 @@
 $(function(){
 	
+	function stopVideosHack(){
+
+		var c = $(this),
+			pr = c.next(),
+			p = c.parent();
+
+		if (!pr.length) {
+			c.appendTo(p);
+		} else {
+			c.insertBefore(pr);
+		}
+
+	}
+	
 	function showPage(){
 		$('body').scrollTop(0)
 		var view = location.hash.substr(1),
 			base = $('#wrapper-all,#footer');
 
+		$('.subpage:visible iframe').each(stopVideosHack);
 		$('.subpage').hide().removeClass('hidden');
+		
 		if(view){
+			base.find(':not(.subpage) * iframe').each(stopVideosHack);
 			base.hide();
 			$('#'+view).show();
 			return;
