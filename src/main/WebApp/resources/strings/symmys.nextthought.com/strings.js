55ce363faaaad5c49b0091789d63d0ae1c90bd63
Jonathan Grimes <jonathan.grimes@nextthought.com>
2014-08-13 17:34:15 +0000

    Clean up Symmys title
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtLoginApp/trunk@46432 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/main/WebApp/resources/strings/symmys.nextthought.com/strings.js
+++ src/main/WebApp/resources/strings/symmys.nextthought.com/strings.js
@@ -1,4 +1,4 @@
 window.NTIStrings = window.NTIStrings || {};
-NTIStrings['application.title-bar-prefix'] = 'SYMMYS: ';
+NTIStrings['application.title-bar-prefix'] = 'SYMMYS';
 //Define externalized strings using the following form
 //NTIStrings['key'] = 'value'
