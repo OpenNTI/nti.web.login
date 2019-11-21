46c7cb9baf0dfd35f687c762a92ab25f8dc3f14b
Andrew Ligon <andrew.ligon@nextthought.com>
2019-11-21 16:14:58 -0600

    better oauth button support

--- src/main/js/app/login/methods/oauth/Button.jsx
+++ src/main/js/app/login/methods/oauth/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
+import {scoped} from '@nti/lib-locale';
 
 import {Button} from '../../../../common';
 
@@ -8,6 +9,32 @@ import Styles from './Styles.css';
 
 const cx = classnames.bind(Styles);
 const getServiceName = rel => rel.split('.')[1].toLowerCase();
+const t = scoped('nti-login.login.methods.oauth.Button', {
+	text: {
+		google: 'Sign In With Google',
+		facebook: 'Sign In With Facebook',
+		linkedin: 'Sign In With LinkedIn'
+	}
+});
+
+const overrides = {
+	google: {
+		background: '#c94b36',
+		title: t('text.google'),
+		theme: 'light'
+	},
+	facebook: {
+		background: '#4469a5',
+		title: t('text.facebook'),
+		theme: 'light'
+	},
+	linkedin: {
+		background: '#0085ae',
+		title: t('text.linkedin'),
+		theme: 'light'
+	}
+};
+
 
 OauthButton.propTypes = {
 	link: PropTypes.shape({
@@ -26,9 +53,29 @@ export default function OauthButton ({link, returnURL}) {
 	url.searchParams.set('success', returnURL || base);
 	url.searchParams.set('failure', base);
 
+	const service = getServiceName(rel);
+
+	const override = overrides[service] || {};
+	const text = override.title || title || service;
+
+	const extraProps = {};
+
+	if (override.background) {
+		extraProps.background = override.background;
+	}
+
+	if (override.theme) {
+		extraProps.theme = override.theme;
+	}
+
 	return (
-		<Button as="a" href={url.toString()} className={cx('oauth-button', getServiceName(rel))}>
-			{title}
+		<Button
+			as="a"
+			href={url.toString()}
+			className={cx('oauth-button', getServiceName(rel))}
+			{...extraProps}
+		>
+			{text}
 		</Button>
 	);
 }
