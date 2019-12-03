3b11c39db82bc8832a21f5ac8789fdac5e9f04a7
Andrew Ligon <andrew.ligon@nextthought.com>
2019-12-03 13:46:28 -0600

    the unsupported page needs to be static

--- src/main/js/app/View.jsx
+++ src/main/js/app/View.jsx
@@ -11,8 +11,6 @@ import './View.css';
 import Login from './login';
 import Recover from './recover';
 import Signup from './signup';
-import Unsupported from './unsupported';
-
 
 export default function LoginApp () {
 	const basePath = getConfigFor('basepath');
@@ -20,7 +18,6 @@ export default function LoginApp () {
 	return (
 		<Theme.Apply theme={LoginTheme.getTheme()}>
 			<Router basepath={basePath} >
-				<Unsupported path="unsupported" />
 				<Page component={Recover} path="recover/*" scope="recover" position={3} />
 				<Page component={Signup} path="signup" scope="signup"  position={2} />
 				<Page component={Login} path="/" scope="login" position={1} />
