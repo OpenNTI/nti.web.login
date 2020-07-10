276c23e13cb23a8f72141d0854f41e190872f385
Andrew Ligon <andrew.ligon@nextthought.com>
2020-07-10 13:46:23 -0500

    NTI-9488 try and not mess up a hash on the return URL

--- src/main/js/data/get-login-redirect-url.js
+++ src/main/js/data/get-login-redirect-url.js
@@ -1,6 +1,20 @@
 import getReturnURL from './get-return-url';
 
 export default function getLoginRedirectURL () {
-	//TODO: be smarter about this;
-	return `${getReturnURL()}?_u=42`;
+	const returnURL = getReturnURL();
+
+	try {
+		//This will throw if the returnURL isn't a fully resolved url,
+		//later we might want to change it to new URL(returnURl, global.location.href)
+		//so it is always fully resolved. But in an attempt to keep the return value
+		//the same as when it was just `return ${returnURL}?_u=42` we're going to
+		//let it throw for now
+		const url = new URL(returnUrl);
+		
+		url.searchParams.set('_u', 42);
+
+		return url.toString();
+	} catch (e) {
+		return `${returnURL}?_u=42`;
+	}
 }
