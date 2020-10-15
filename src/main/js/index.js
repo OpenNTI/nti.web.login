a772df834705841e20e17e7cd1b2105426939872
Jonathan Grimes <jsg2021@gmail.com>
2020-10-15 13:31:48 -0500

    NTI-9693 - Make login app support site-assets

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {addFeatureCheckClasses} from '@nti/lib-dom';
+import {init as initLocale} from '@nti/lib-locale';
 
 import App from './app';
 
 addFeatureCheckClasses();
+initLocale();
 
 function onAppMount (APP) {
 	global.appInitialized = !!APP;
