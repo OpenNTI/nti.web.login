45601f9a70c62c51145cdd52b7e0494e8d88b6cd
Jonathan Grimes <jsg2021@gmail.com>
2020-12-10 08:41:40 -0600

    NTI-9989 - add frame busting

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -8,6 +8,10 @@ import App from './app';
 addFeatureCheckClasses();
 initLocale();
 
+if (window.top === window) {
+	window.top.location.replace(window.location);
+}
+
 function onAppMount (APP) {
 	global.appInitialized = !!APP;
 }
