e67503a3cc640df08b424a27fd1d42eff3bac255
Jonathan Grimes <jsg2021@gmail.com>
2021-02-16 17:56:06 -0600

    add error reporter

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {addFeatureCheckClasses} from '@nti/lib-dom';
-import {init as initLocale} from '@nti/lib-locale';
+import { addFeatureCheckClasses } from '@nti/lib-dom';
+import { init as initLocale } from '@nti/lib-locale';
+import { initErrorReporter } from '@nti/web-client';
+
+initErrorReporter();
 
 import App from './app';
 
 addFeatureCheckClasses();
 initLocale();
 
-function onAppMount (APP) {
+function onAppMount(APP) {
 	global.appInitialized = !!APP;
 }
 
 ReactDOM.render(
-	React.createElement(App, {ref: onAppMount}),
+	React.createElement(App, { ref: onAppMount }),
 	document.getElementById('content')
 );
