50e28cbdd3a8d3d218cbd03bfe3bfee5f39f09b0
Jonathan Grimes <jsg2021@gmail.com>
2021-03-23 18:40:30 -0500

    use named link

--- src/main/js/data/get-terms-link.js
+++ src/main/js/data/get-terms-link.js
@@ -1,12 +1,12 @@
 import { getPong } from './get-pong';
 
-const TermsLink =
-	'https://docs.google.com/document/d/e/2PACX-1vRJd0Irh_YFX7Ci9irWLmqrEqddrxSLrDkrJMANlCqQAo-PrLznTjk4G0hfCsjxD8M21Vd54iQ1Rqbn/pub';
-
 export async function getTermsLink() {
 	try {
 		const pong = await getPong();
-		return pong.getLink('terms-of-service') || TermsLink;
+		return (
+			pong.getLink('terms-of-service') ||
+			pong.getLink('content.permanent_tos_page')
+		);
 	} catch (e) {
 		return null;
 	}
