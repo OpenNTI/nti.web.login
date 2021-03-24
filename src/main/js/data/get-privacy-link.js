a16dacbd43b3d4ba7f404a7720b6da8172c74896
Jonathan Grimes <jsg2021@gmail.com>
2021-03-24 17:07:01 -0500

    NTI-10436 - look for terms/privacy policy in additional places

--- src/main/js/data/get-privacy-link.js
+++ src/main/js/data/get-privacy-link.js
@@ -4,8 +4,9 @@ export async function getPrivacyLink() {
 	try {
 		const pong = await getPong();
 		return (
-			pong.getLink('privacy-policy') ||
-			pong.getLink('content.permanent_general_privacy_page')
+			pong.getLink('privacy-policy') || //placeholder "future override"
+			pong.getLink('content.permanent_general_privacy_page') ||
+			pong.getLink('content.direct_privacy_link')
 		);
 	} catch (e) {
 		return null;
--- src/main/js/data/get-terms-link.js
+++ src/main/js/data/get-terms-link.js
@@ -4,8 +4,9 @@ export async function getTermsLink() {
 	try {
 		const pong = await getPong();
 		return (
-			pong.getLink('terms-of-service') ||
-			pong.getLink('content.permanent_tos_page')
+			pong.getLink('terms-of-service') || //placeholder "future override"
+			pong.getLink('content.permanent_tos_page') ||
+			pong.getLink('content.direct_tos_link')
 		);
 	} catch (e) {
 		return null;
