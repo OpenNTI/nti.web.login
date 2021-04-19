37ab2a7b645506f60ccd99d0b484d8702708a74c
Jonathan Grimes <jsg2021@gmail.com>
2021-04-19 10:19:32 -0500

    lint --fix

--- src/main/js/data/Cookies.js
+++ src/main/js/data/Cookies.js
@@ -1,6 +1,7 @@
 /**
  * Return a value for a given cookie name
  * https://stackoverflow.com/questions/5639346/what-is-the-shortest-function-for-reading-a-cookie-by-name-in-javascript
+ *
  * @param  {string} name the cookie to look up
  * @returns {string}      the cookie value
  */
