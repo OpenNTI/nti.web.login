9ce13175a4ff7d33dfb4c47a8ef57cffb173107f
Ray Hatfield <ray.hatfield@gmail.com>
2021-05-17 14:23:16 -0500

    wip catalog routing

--- src/main/js/app/catalog/View.jsx
+++ src/main/js/app/catalog/View.jsx
@@ -1,5 +1,10 @@
-import { Router, Route } from '@nti/web-routing';
+import { Suspense, useEffect, useState } from 'react';
+import { Router } from '@reach/router';
+
+import { Router as NTIRouter } from '@nti/web-routing';
 import Catalog from '@nti/web-catalog';
+import { useService } from '@nti/web-commons';
+import { Info } from '@nti/web-course';
 
 const getRouteFor = obj => {
 	if ((obj?.isCourseCatalogEntry || obj?.isCourse) && obj.getID?.()) {
@@ -7,10 +12,57 @@ const getRouteFor = obj => {
 	}
 };
 
-export const CatalogView = Router.for([
-	Route({
-		path: '/*',
-		getRouteFor,
-		component: Catalog,
-	}),
-]);
+const Container = styled('div')`
+	max-width: 1024px;
+	margin: 0 auto;
+`;
+
+const Detail = ({ id, ...other }) => {
+	const service = useService();
+	const [catalogEntry, setCatalogEntry] = useState();
+
+	useEffect(() => {
+		async function loadCatalogEntry() {
+			const entry = await service.getObject(id);
+			setCatalogEntry(entry);
+		}
+
+		loadCatalogEntry();
+	}, [id]);
+
+	return (
+		<Container>
+			{!catalogEntry ? (
+				<span>Loading</span>
+			) : (
+				<Info.Inline catalogEntry={catalogEntry} />
+			)}
+		</Container>
+	);
+};
+
+// const CatalogRoutes = Router.for([
+// 	Route({
+// 		path: '/login/catalog/nti-course-catalog-entry/:id',
+// 		getRouteFor,
+// 		component: Detail,
+// 	}),
+// 	Route({
+// 		path: '/*',
+// 		getRouteFor,
+// 		component: Catalog,
+// 	}),
+// ]);
+
+export function CatalogView(props) {
+	return (
+		<Suspense fallback={<div />}>
+			<NTIRouter.RouteForProvider getRouteFor={getRouteFor}>
+				<Router>
+					<Detail path="/nti-course-catalog-entry/:id" />
+					<Catalog path="/*" />
+				</Router>
+			</NTIRouter.RouteForProvider>
+		</Suspense>
+	);
+}
