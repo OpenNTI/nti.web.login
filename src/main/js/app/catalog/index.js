120bc52547411934e308d047a58050f34f1de04a
Andrew Ligon <aligon90@gmail.com>
2021-05-25 16:41:23 -0500

    explain strange code

--- src/main/js/app/catalog/index.js
+++ src/main/js/app/catalog/index.js
@@ -6,9 +6,15 @@ import { getHistory } from '@nti/web-routing';
 
 const webRoutingHistory = getHistory();
 
-export function Catalog(props) {
+/**
+ * reach/router and nti/web-routing both push to the browsers history object,
+ * and neither listen for changes to update their state... This hook is setup
+ * so that changes to one router will be represented in the other.
+ */
+function useRouterBridge() {
 	const location = useLocation();
 
+	//When nti/web-routing changes
 	useEffect(() => {
 		return webRoutingHistory.listen(({ pathname, state }) => {
 			if (globalHistory.location?.pathname !== pathname) {
@@ -17,11 +23,16 @@ export function Catalog(props) {
 		});
 	}, []);
 
+	//When reach/router changes
 	useEffect(() => {
 		if (location.pathname !== webRoutingHistory.pathname) {
 			webRoutingHistory.replace(location.pathname);
 		}
 	}, [location.pathname]);
+}
+
+export function Catalog(props) {
+	useRouterBridge();
 
 	return <AnonymousCatalog {...props} />;
 }
