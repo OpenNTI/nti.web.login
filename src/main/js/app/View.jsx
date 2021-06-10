8c48fc91582eb233e5ad9a98ac9643fa4fe502bb
ray hatfield <ray.hatfield@nextthought.com>
2021-06-10 10:03:33 -0500

    NTI-10711 - anonymous catalog views (#27)
    * remove unused position prop
    
    * add catalog routes/view
    
    * wip catalog routing
    
    * pass the baseroute to the Catalog component
    
    * move catalog logic to web-catalog; wip on bridging routers
    
    * sync reach-router and web-router
    
    * explain strange code
    
    * pass nav info to anonymous catalog
    
    * logon.continue-anonymously is not an oauth link
    
    * lazy-load the catalog chunk
    
    Co-authored-by: Andrew Ligon <aligon90@gmail.com>

--- src/main/js/app/View.jsx
+++ src/main/js/app/View.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Router } from '@reach/router';
 
 import { getConfig } from '@nti/web-client';
@@ -14,40 +14,51 @@ import Recover from './recover';
 import Signup from './signup';
 import AcceptInvite from './accept-invite';
 
+const Catalog = lazy(() =>
+	/* webpackChunkName: "catalog" */ import('./catalog')
+);
+
+const LOGIN = '/';
+const SIGNUP = 'signup';
+
 export default React.forwardRef(function LoginApp(props, ref) {
 	const basePath = getConfig('basepath');
 	React.useImperativeHandle(ref, () => ({}));
 
+	const PATHS = {
+		login: `${basePath}${LOGIN}`,
+		signup: `${basePath}${SIGNUP}`,
+	};
+
 	return (
 		<Theme.Apply theme={LoginTheme.getTheme()}>
-			<Router basepath={basePath}>
-				<Page
-					component={Recover}
-					path="recover/*"
-					scope="recover"
-					position={3}
-				/>
-				<Page
-					component={Signup}
-					path="signup"
-					scope="signup"
-					position={2}
-				/>
-				<Page
-					component={AcceptInvite}
-					path="account-setup/*"
-					scope="accountSetup"
-					position={2}
-					isAccountSetup
-				/>
-				<Page
-					component={AcceptInvite}
-					path="accept-invite/*"
-					scope="acceptInvitation"
-					position={2}
-				/>
-				<Page component={Login} path="/" scope="login" position={1} />
-			</Router>
+			<Suspense fallback={<div />}>
+				<Router basepath={basePath}>
+					<Page
+						component={Recover}
+						path="recover/*"
+						scope="recover"
+					/>
+					<Page component={Signup} path={SIGNUP} scope="signup" />
+					<Page
+						component={AcceptInvite}
+						path="account-setup/*"
+						scope="accountSetup"
+						isAccountSetup
+					/>
+					<Page
+						component={AcceptInvite}
+						path="accept-invite/*"
+						scope="acceptInvitation"
+					/>
+					<Catalog
+						path="catalog/*"
+						baseroute={`${basePath}catalog`}
+						paths={PATHS}
+					/>
+					<Page component={Login} path={LOGIN} scope="login" />
+				</Router>
+			</Suspense>
 		</Theme.Apply>
 	);
 });
---
+++ src/main/js/app/catalog/index.js
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { navigate, globalHistory, useLocation } from '@reach/router';
+
+import { AnonymousPage as AnonymousCatalog } from '@nti/web-catalog';
+import { getHistory } from '@nti/web-routing';
+
+const webRoutingHistory = getHistory();
+
+/**
+ * reach/router and nti/web-routing both push to the browsers history object,
+ * and neither listen for changes to update their state... This hook is setup
+ * so that changes to one router will be represented in the other.
+ */
+function useRouterBridge() {
+	const location = useLocation();
+
+	//When nti/web-routing changes
+	useEffect(() => {
+		return webRoutingHistory.listen(({ pathname, state }) => {
+			if (globalHistory.location?.pathname !== pathname) {
+				navigate(pathname, { replace: true });
+			}
+		});
+	}, []);
+
+	//When reach/router changes
+	useEffect(() => {
+		if (location.pathname !== webRoutingHistory.pathname) {
+			webRoutingHistory.replace(location.pathname);
+		}
+	}, [location.pathname]);
+}
+
+export function Catalog(props) {
+	useRouterBridge();
+
+	return <AnonymousCatalog {...props} />;
+}
+
+export default Catalog; // React.lazy only supports default exports
