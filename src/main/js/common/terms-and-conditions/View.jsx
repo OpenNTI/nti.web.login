d5f5eea1d8118bf9676351c66778e911278197c8
Andrew Ligon <aligon90@gmail.com>
2021-07-01 14:24:07 -0500

    NTI-10895 getPrivacyLink and getTermsLink are async now

--- src/main/js/common/terms-and-conditions/View.jsx
+++ src/main/js/common/terms-and-conditions/View.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 
+import { Hooks } from '@nti/web-commons';
 import { getPrivacyLink, getTermsLink } from 'internal/data';
 
 import Text from '../text';
 
+const { useResolver } = Hooks;
+const { isPending, isResolved } = useResolver();
+
 export default function TermsAndConditions() {
+	const resolver = useResolver(async () => {
+		return {
+			privacyLink: await getPrivacyLink(),
+			termsLink: await getTermsLink(),
+		};
+	}, []);
+
+	const loading = isPending(resolver);
+	const { privacyLink, termsLink } = isResolved(resolver) ? resolver : {};
+
+	if (loading) {
+		return null;
+	}
+
 	return (
 		<Text.Medium center>
 			By creating an account, you agree to <br />{' '}
-			<a href={getTermsLink()} target="_blank" rel="noopener noreferrer">
+			<a href={termsLink} target="_blank" rel="noopener noreferrer">
 				Terms of Use
 			</a>{' '}
 			and{' '}
-			<a
-				href={getPrivacyLink()}
-				target="_blank"
-				rel="noopener noreferrer"
-			>
+			<a href={privacyLink} target="_blank" rel="noopener noreferrer">
 				Privacy Policy.
 			</a>
 		</Text.Medium>
