6d24d5fae18fbbec3d0d2c52522dd01512bc36c9
Jonathan Grimes <jsg2021@gmail.com>
2021-10-19 10:02:49 -0500

    NTI-11195 - do not build bad urls

--- src/main/js/data/get-login-redirect-url.js
+++ src/main/js/data/get-login-redirect-url.js
@@ -4,17 +4,13 @@ export function getLoginRedirectURL() {
 	const returnURL = getReturnURL();
 
 	try {
-		//This will throw if the returnURL isn't a fully resolved url,
-		//later we might want to change it to new URL(returnURl, global.location.href)
-		//so it is always fully resolved. But in an attempt to keep the return value
-		//the same as when it was just `return ${returnURL}?_u=42` we're going to
-		//let it throw for now
-		const url = new URL(returnURL);
+		const url = new URL(returnURL, global.location.href);
 
-		url.searchParams.set('_u', 42);
+		//cache bust
+		url.searchParams.set('_u', Math.round(Math.random() * 100));
 
 		return url.toString();
 	} catch (e) {
-		return `${returnURL}?_u=42`;
+		return returnURL;
 	}
 }
